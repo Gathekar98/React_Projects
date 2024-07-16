36c3e21db2bc1880043d17b6ecaedb72905000a8
Gathekar98 <gathekar.ashwini@gmail.com>
2024-07-16 14:58:25 +0530

    Basic-Static website first page by using React

---
+++ Hero_Shoes/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./components/Hero";
+import Navigation from "./components/Navigation";
+const App = () => {
+  return (
+    <div className="container">
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
